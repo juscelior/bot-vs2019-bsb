03fb1febc959e398d46bbc1cc5b0f95740e80af4
Juscélio Reis <juscelioreis@wizsolucoes.com.br>
2019-04-17 13:50:19 -0300

    'inico'

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=vs-2019-demo-bsb-Web-Deploy.pubxml -p:Password=dkSYtbvLZqJjY349mMKAA7dGfCcMATqCd2vYdCDyop9QpkvSjPce0LvHYp7l
+
